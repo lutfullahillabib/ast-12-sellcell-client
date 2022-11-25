fa1534a463f019188d0ecce8ce3cb598a75a5ac7
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-26 00:36:28 +0600

    private route

---
+++ src/Routes/PrivateRoute.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../Contexts/AuthProvider';
+
+const PrivateRoute = ({ children }) => {
+
+    const { user, loading } = useContext(AuthContext);
+
+    const location = useLocation();
+
+    if (loading) {
+        return <div className='flex flex-col gap-3 justify-center items-center h-[70vh]'>
+
+            <progress className="progress w-8/12 h-6 progress-primary" ></progress>
+            <progress className="progress w-8/12 h-6 progress-secondary" ></progress>
+            <progress className="progress w-8/12 h-6 progress-accent" ></progress>
+
+            <progress className="progress w-8/12 h-6 progress-success" ></progress>
+            <progress className="progress w-8/12 h-6 progress-info" ></progress>
+
+            <progress className="progress w-8/12 h-6 progress-warning" ></progress>
+            <progress className="progress w-8/12 h-6 progress-error" ></progress>
+
+        </div>
+
+    }
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
+
+
